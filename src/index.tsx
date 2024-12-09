53e96e7f2fa012ea3aab8da1c43b30ecd058670a
mrsvolodya <vovamursky@gmail.com>
2024-12-09 08:15:49 +0200

    finish

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,6 @@ import { HashRouter as Router } from "react-router-dom";
 import { RecipeProvider } from "./store/RecipeProvider";
 import { Root } from "./Root";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 const queryClient = new QueryClient();
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -15,7 +14,6 @@ root.render(
     <Router>
       <QueryClientProvider client={queryClient}>
         <Root />
-        <ReactQueryDevtools initialIsOpen={true} />
       </QueryClientProvider>
     </Router>
   </RecipeProvider>
