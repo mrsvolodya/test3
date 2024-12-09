388b1cd889f567463862826ce4c9f2ab424566df
mrsvolodya <vovamursky@gmail.com>
2024-12-09 12:33:41 +0200

    add local storage

--- src/store/RecipeProvider.tsx
+++ src/store/RecipeProvider.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 import { ProviderProps } from "../types/ProviderProps";
 import { Meal } from "../types/Meal";
 import { ContextProps } from "../types/ContextProps";
@@ -22,16 +22,36 @@ const RecipeProvider: React.FC<ProviderProps> = ({ children }) => {
     return recipes.some((recipe: Meal) => recipe.idMeal === idMeal);
   };
 
+  const addToLocalStorage = (key: string, recipe: Meal[]) => {
+    localStorage.setItem(key, JSON.stringify(recipe));
+  };
+
   const hanleSelectedRecipes = (recipe: Meal) => {
     setSelectedRecipes((prevRecipes) => {
       const isRecipeInCart = isInCart(prevRecipes, recipe.idMeal);
       if (isRecipeInCart) {
-        return prevRecipes.filter((rec) => rec.idMeal !== recipe.idMeal);
+        const filteredRecipes = prevRecipes.filter(
+          (rec) => rec.idMeal !== recipe.idMeal
+        );
+
+        addToLocalStorage("selectedRecipes", filteredRecipes);
+        return filteredRecipes;
       } else {
-        return [...prevRecipes, recipe];
+        const addNewRecipe = [...prevRecipes, recipe];
+        addToLocalStorage("selectedRecipes", addNewRecipe);
+
+        return addNewRecipe;
       }
     });
   };
+
+  useEffect(() => {
+    const favorites = localStorage.getItem("selectedRecipes");
+    if (favorites) {
+      const parsedFavorites = JSON.parse(favorites);
+      setSelectedRecipes(parsedFavorites);
+    }
+  }, []);
 
   return (
     <RecipeContext.Provider
